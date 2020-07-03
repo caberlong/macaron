ba7455e7d7467c79c1a66f27580f40c86fbb8030
Caber Long <67236363+caberlong@users.noreply.github.com>
2020-07-02 21:52:57 -0700

    Add an antlr example.

---
+++ examples/antlr/hello/hello.py
@@ -0,0 +1,19 @@
+import sys
+from antlr4 import *
+from HelloLexer import HelloLexer
+from HelloParser import HelloParser
+
+#from examples.antlr.hello.HelloLexer import HelloLexer
+#from examples.antlr.hello.HelloParser import HelloParser
+
+
+def main(argv):
+    input = FileStream(argv[1])
+    lexer = HelloLexer(input)
+    stream = CommonTokenStream(lexer)
+    parser = HelloParser(stream)
+    tree = parser.r()
+    print(tree.toStringTree(recog=parser))
+
+if __name__ == '__main__':
+    main(sys.argv)
